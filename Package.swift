389502d5c7a3ae44b716264cdeaac22a7f30010f
Thomas Roughton <t.roughton@me.com>
2019-09-05 17:27:51 +1200

    Bump minimum supported version to Mojave

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AppFramework",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v10_14)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
